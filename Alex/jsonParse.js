8dcd57fe1bbdff96ab5209f7229570875289fab9
Alex Hosford <alex.hosford@semantico.com>
2012-02-21 23:45:19 +0000

    moved all my stuff into one folder for working with

---
+++ Alex/jsonParse.js
@@ -0,0 +1,26 @@
+$(document).ready(function) {
+	
+	//An array to store all the JSON objects?
+	var people = new Array();
+	
+	//Go through the array and add the JSON objects?
+	//how best to retrieve them, how best to access them and assign them to the indices?
+	for (i = 0; i < people.length; i++) {
+		people[i] = ; //GOOGLEDOCSJSONOBJECT-FINDIDENTIFIER
+	};	
+};
+
+//An example of JSON object creation
+//var JSONObject = {  "name" : "Alex",
+//	"position"  : "graduate developer",
+//	"java"  : l2, 
+//	"html"   : l2,
+//	"css"  : l1
+//};
+
+//This below obviously amends all the HTML
+document.write("<h2><font color='blue'>Name</font>::"+JSONObject.name+"</h2>");  
+document.write("<h2><font color='blue'>Address</font>::"+JSONObject.address+"</h2>");  
+document.write("<h2><font color='blue'>Age</font>::"+JSONObject.age+"</h2>");  
+document.write("<h2><font color='blue'>Phone No.</font>::"+JSONObject.phone+"</h2>");  
+document.write("<h2><font color='blue'>Mobile No.</font>::"+JSONObject.MobileNo+"</h2>"); 
