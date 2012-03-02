6c73eed25c52098be28ab96fde73811f5c4b1a83
Alex Hosford <alex.hosford@semantico.com>
2012-03-02 12:32:49 +0000

    working truncation

--- Alex/SVNRadiatorWithSVNKit/src/main/java/MakePage.java
+++ Alex/SVNRadiatorWithSVNKit/src/main/java/MakePage.java
@@ -3,18 +3,13 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.text.DateFormat;
-import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.StringTokenizer;
-
 import org.tmatesoft.svn.core.SVNLogEntry;
-import org.tmatesoft.svn.core.SVNLogEntryPath;
-
 import freemarker.template.DefaultObjectWrapper;
 import freemarker.template.Template;
 import freemarker.template.Configuration;
@@ -26,110 +21,90 @@ public class MakePage {
 	
 	String[] author = new String[8];
 	String[] message = new String[8];
-	
 	String[] dateString = new String[8];
-	Date date;
-	String time;
-	
 	String[] projectName = new String[8];
-	String pathsString;
-	Map paths; //Just need to check that what I have done below is also valid and that I can avoid a a warning here by doing the toString() method in one movement 
-	String paths2;
-	
+	String trunc = "...";
 	//for the model
 	Map<String, Map<String, String>> root;
 	//for the page				
 	Template page;
 
+	//Gets passed the LogEntry Collection from the radiation page
 	public MakePage(Collection<SVNLogEntry> log) throws IOException {
-		
 		logEntries = log;
-		
-		//Create configuration instance
 		Configuration cfg = new Configuration();
-		//Where I'm storing the template
 		cfg.setDirectoryForTemplateLoading(new File("/Users/panda/SemanticoRadiators/Alex/SVNRadiatorWithSVNKit/"));
-		//How we see the data model
 		cfg.setObjectWrapper(new DefaultObjectWrapper());
-		//set the freemarker template
 		page = cfg.getTemplate("OpenProjects.ftl");
-		
 		getEntryData();
 		createModel();
 	}
 	
 	//Retrieves all of the radiator relevant data from the entry objects
 	public void getEntryData() {
-		
 		Iterator<SVNLogEntry> it = logEntries.iterator();
-		int i = 0;	//have to specify a count here because I need it
+		int i = 0;
 		while (it.hasNext()) {
-
 			SVNLogEntry entry = it.next();
-
-			// System.out.println(entry.getAuthor());
-			// System.out.println(entry.getDate());
-			// System.out.println(entry.getMessage());
-			// System.out.println(entry.getChangedPaths());
-			
 			//These two are strings, no extra formatting needed
-			author[i] = entry.getAuthor();
-			message[i] = entry.getMessage();	//So simple that this doesn't really require moving into its own method
-			
-			
-			//This one returns a date - make it into a string
-			date = entry.getDate();
-			//dateString[i] = date.toString();
-			//time = createTime(date);	//should try to move all this to date method below
-			dateString[i] = createTime(date);			
-			
-			//Returns a Map object
-			paths = entry.getChangedPaths();
-			paths2 = entry.getChangedPaths().toString();
-			//Convert it to a string
-			pathsString = paths.toString();
-			//Parses the string to get project name using defined method below
-			projectName[i] = createProjectName(pathsString);
-
+			projectName[i] = truncateProjectName(createProjectName(entry.getChangedPaths().toString()));
+			author[i] = truncateName(entry.getAuthor());
+			dateString[i] = createTime(entry.getDate());	//extracts a time String from the Date object			
+			message[i] = truncateMessage(entry.getMessage()); //truncates the message to 120 characters
 			i++;
 		}
 	}
-	
-	//or - should this take a date object directly, use the date methods and then return a string? This certainyl makes sense in terms of splitting up functionality in own methods.
-	private String createTime(Date theDate) {
-		String theTime, newTime;
-		theTime  = DateFormat.getTimeInstance().format(theDate).toString();
-		StringBuilder sb = new StringBuilder(theTime);
-		newTime = sb.delete((sb.length()-3), sb.length()).toString();
-		return newTime; 
-		
-	}
 
-	//This takes a large string relating to the path of the object and takes the first name, resulting in a project name
-	public String createProjectName(String chPaths) {
-		
-		Scanner pscan = new Scanner(chPaths).useDelimiter("/");
-		@SuppressWarnings("unused") //This is only here because I know 'slash' is an unused variable. As far as I could see the next() method does not take an integer as an argument so i was not sure how to skip the first character otherwise
-		String slash = pscan.next();
-		String pName = pscan.next();
-		return pName;
+	//Truncates messages to 120 characters
+	private String truncateMessage(String aMessage) {
+		StringBuilder messageString = new StringBuilder(aMessage);
+		if(messageString.length()>119) {
+			return messageString.delete(116, messageString.length()).toString()+trunc;
+		}
+		return messageString.toString();
 	}
 	
+	//Exctracts a time String from a Date object
+	private String createTime(Date theDate) {
+		StringBuilder timeString = new StringBuilder(DateFormat.getTimeInstance().format(theDate).toString());
+		return timeString.delete((timeString.length()-3), timeString.length()).toString();
+	}
+	
+	//Pulls the project name out of the project path
+	public String createProjectName(String chPaths) {
+		Scanner pscan = new Scanner(chPaths).useDelimiter("/");
+		pscan.next(); //skip the slash
+		return pscan.next();
+	}
+	
+	//Truncates the project name if it's more than 18 characters
+	public String truncateProjectName(String longProjName) {
+		StringBuilder projString = new StringBuilder(longProjName);
+		if(projString.length()>17) {
+			return projString.delete(14, projString.length()).toString()+trunc;
+		}
+		return projString.toString(); 
+	}
+	
+	//Truncates the username if it's more than 10 characters
+	public String truncateName(String longName) {
+		StringBuilder nameString = new StringBuilder(longName);
+		if(nameString.length()>9) {
+			return nameString.delete(6, nameString.length()).toString()+trunc;
+		}
+		return nameString.toString();
+	}
 	//Builds to freemarker model with the corresponding entry object data
 	public void createModel() {
-		
 		//create root map
 		root = new HashMap<String, Map<String, String>>();
-	
 		//loops through and makes 10 revision maps for the model, each with their own identifier
 		for(int i = 0; i < 8; i++) {
-			
 			String revisionName = "rev"+(i+1);
 			//map for revision
 			Map<String, String> revision = new HashMap<String, String>();
 			//put it in root map
 			root.put(revisionName, revision);
-			
 			//add the SVNENtry values
 			revision.put("author", author[i]);			
 			revision.put("date", dateString[i]);
@@ -140,7 +115,6 @@ public class MakePage {
 	
 	//This takes the data-model and the template and merges them together
 	public void fillPage() throws TemplateException, IOException {
-		
 		//at the moment this is making an output stream but I need this to make a file. This is the final piece of the jigsaw I think!
 		Writer out = new OutputStreamWriter(System.out);
 		page.process(root,out);
--- Alex/SVNRadiatorWithSVNKit/src/main/java/Radiation.java
+++ Alex/SVNRadiatorWithSVNKit/src/main/java/Radiation.java
@@ -8,11 +8,7 @@ import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
 import org.tmatesoft.svn.core.io.SVNRepository;
 import org.tmatesoft.svn.core.wc.DefaultSVNRepositoryPool;
 import org.tmatesoft.svn.core.wc.SVNWCUtil;
-
 import freemarker.template.TemplateException;
-
-//Another attempt at grabbing revision histories from Subversion.
-//This one makes use of the SVNKit library.
 
 public class Radiation {
 
@@ -28,21 +24,17 @@ public class Radiation {
 	private static Collection<SVNLogEntry> log;
 	
 	public static void main(String[] args) throws SVNException, IOException, TemplateException {
-		
 		initialSetup();
-		
 		//Returns the latest revision number for the repository.
 		latestRev = repository.getLatestRevision();
 		//Calculates the number of the 8th one ago. This is 7 because the number of revisions to display is inclusive of the latest one
 		tenRevs = latestRev - 7;
-		
 		makeEntries();
 		updatePage();
 	}
 	
 	//This sets everything up so that the SVNKit will play nice.
 	private static void initialSetup() throws SVNException {
-		
 		//For working nicely over http:// and https://.
 		DAVRepositoryFactory.setup();		
 		//For authentication.
@@ -55,14 +47,12 @@ public class Radiation {
 	
 	//Makes a collection of log entries from the repositiory.
 	public static void makeEntries() throws SVNException, IOException {
-		
 		//This returns a collection of logEntry objects. Needs an array of paths to look in (empty String[]), start and end revision also passed in.
-		log = repository.log(new String[]{}, null, tenRevs, latestRev, true, false);
+		log = repository.log(new String[]{}, null, tenRevs, latestRev, true, false); //This is throwing a warning because of Object types, but this really can't be avoided right now.
 	}
 	
 	//Calls the MakePage class to sort out the radiator page.
 	public static void updatePage() throws IOException, TemplateException {
-		
 		MakePage model = new MakePage(log);
 		model.fillPage();
 	}
