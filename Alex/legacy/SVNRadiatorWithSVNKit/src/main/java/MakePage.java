d4e2db85ecd237f29e10c6f407b3e6c89919d967
Alex Hosford <alex.hosford@semantico.com>
2012-03-02 16:20:59 +0000

    working on dynamic web version of working subversionRadiator

---
+++ Alex/legacy/SVNRadiatorWithSVNKit/src/main/java/MakePage.java
@@ -0,0 +1,124 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.text.DateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import org.tmatesoft.svn.core.SVNLogEntry;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+import freemarker.template.Configuration;
+import freemarker.template.TemplateException;
+
+public class MakePage {
+	
+	Collection<SVNLogEntry> logEntries;
+	
+	String[] author = new String[8];
+	String[] message = new String[8];
+	String[] dateString = new String[8];
+	String[] projectName = new String[8];
+	String trunc = "...";
+	//for the model
+	Map<String, Map<String, String>> root;
+	//for the page				
+	Template page;
+
+	//Gets passed the LogEntry Collection from the radiation page
+	public MakePage(Collection<SVNLogEntry> log) throws IOException {
+		logEntries = log;
+		Configuration cfg = new Configuration();
+		cfg.setDirectoryForTemplateLoading(new File("/Users/panda/SemanticoRadiators/Alex/SVNRadiatorWithSVNKit/"));
+		cfg.setObjectWrapper(new DefaultObjectWrapper());
+		page = cfg.getTemplate("OpenProjects.ftl");
+		getEntryData();
+		createModel();
+	}
+	
+	//Retrieves all of the radiator relevant data from the entry objects
+	public void getEntryData() {
+		Iterator<SVNLogEntry> it = logEntries.iterator();
+		int i = 0;
+		while (it.hasNext()) {
+			SVNLogEntry entry = it.next();
+			//These two are strings, no extra formatting needed
+			projectName[i] = truncateProjectName(createProjectName(entry.getChangedPaths().toString()));
+			author[i] = truncateName(entry.getAuthor());
+			dateString[i] = createTime(entry.getDate());	//extracts a time String from the Date object			
+			message[i] = truncateMessage(entry.getMessage()); //truncates the message to 120 characters
+			i++;
+		}
+	}
+
+	//Truncates messages to 120 characters
+	private String truncateMessage(String aMessage) {
+		StringBuilder messageString = new StringBuilder(aMessage);
+		if(messageString.length()>119) {
+			return messageString.delete(116, messageString.length()).toString()+trunc;
+		}
+		return messageString.toString();
+	}
+	
+	//Exctracts a time String from a Date object
+	private String createTime(Date theDate) {
+		StringBuilder timeString = new StringBuilder(DateFormat.getTimeInstance().format(theDate).toString());
+		return timeString.delete((timeString.length()-3), timeString.length()).toString();
+	}
+	
+	//Pulls the project name out of the project path
+	public String createProjectName(String chPaths) {
+		Scanner pscan = new Scanner(chPaths).useDelimiter("/");
+		pscan.next(); //skip the slash
+		return pscan.next();
+	}
+	
+	//Truncates the project name if it's more than 18 characters
+	public String truncateProjectName(String longProjName) {
+		StringBuilder projString = new StringBuilder(longProjName);
+		if(projString.length()>17) {
+			return projString.delete(14, projString.length()).toString()+trunc;
+		}
+		return projString.toString(); 
+	}
+	
+	//Truncates the username if it's more than 10 characters
+	public String truncateName(String longName) {
+		StringBuilder nameString = new StringBuilder(longName);
+		if(nameString.length()>9) {
+			return nameString.delete(6, nameString.length()).toString()+trunc;
+		}
+		return nameString.toString();
+	}
+	//Builds to freemarker model with the corresponding entry object data
+	public void createModel() {
+		//create root map
+		root = new HashMap<String, Map<String, String>>();
+		//loops through and makes 10 revision maps for the model, each with their own identifier
+		for(int i = 0; i < 8; i++) {
+			String revisionName = "rev"+(i+1);
+			//map for revision
+			Map<String, String> revision = new HashMap<String, String>();
+			//put it in root map
+			root.put(revisionName, revision);
+			//add the SVNENtry values
+			revision.put("author", author[i]);			
+			revision.put("date", dateString[i]);
+			revision.put("message", message[i]);
+			revision.put("project", projectName[i]);
+		}
+	}
+	
+	//This takes the data-model and the template and merges them together
+	public void fillPage() throws TemplateException, IOException {
+		//at the moment this is making an output stream but I need this to make a file. This is the final piece of the jigsaw I think!
+		Writer out = new OutputStreamWriter(System.out);
+		page.process(root,out);
+		out.flush();
+	}
+
+}
---
+++ Alex/legacy/SVNRadiatorWithSVNKit/src/main/java/Radiation.java
@@ -0,0 +1,59 @@
+import java.io.IOException;
+import java.util.Collection;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNLogEntry;
+import org.tmatesoft.svn.core.SVNURL;
+import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager;
+import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
+import org.tmatesoft.svn.core.io.SVNRepository;
+import org.tmatesoft.svn.core.wc.DefaultSVNRepositoryPool;
+import org.tmatesoft.svn.core.wc.SVNWCUtil;
+import freemarker.template.TemplateException;
+
+public class Radiation {
+
+	static SVNRepository repository = null;
+	static String url = "https://svn.semantico.net/repos/main";
+	//Anonymous login for now - should be fine to work over Semantico network?. Haven't tested over VPN
+	static String username = "anonymous";
+	static String password = "anonymous";
+	
+	static long latestRev;
+	static long tenRevs;
+	
+	private static Collection<SVNLogEntry> log;
+	
+	public static void main(String[] args) throws SVNException, IOException, TemplateException {
+		initialSetup();
+		//Returns the latest revision number for the repository.
+		latestRev = repository.getLatestRevision();
+		//Calculates the number of the 8th one ago. This is 7 because the number of revisions to display is inclusive of the latest one
+		tenRevs = latestRev - 7;
+		makeEntries();
+		updatePage();
+	}
+	
+	//This sets everything up so that the SVNKit will play nice.
+	private static void initialSetup() throws SVNException {
+		//For working nicely over http:// and https://.
+		DAVRepositoryFactory.setup();		
+		//For authentication.
+		ISVNAuthenticationManager authManager = SVNWCUtil.createDefaultAuthenticationManager(username, password);
+		//A pool to add the repository to.
+		DefaultSVNRepositoryPool pool = new DefaultSVNRepositoryPool(authManager, null);
+		repository = pool.createRepository(SVNURL.parseURIEncoded(url), true);
+		repository.setAuthenticationManager(authManager);		
+	}
+	
+	//Makes a collection of log entries from the repositiory.
+	public static void makeEntries() throws SVNException, IOException {
+		//This returns a collection of logEntry objects. Needs an array of paths to look in (empty String[]), start and end revision also passed in.
+		log = repository.log(new String[]{}, null, tenRevs, latestRev, true, false); //This is throwing a warning because of Object types, but this really can't be avoided right now.
+	}
+	
+	//Calls the MakePage class to sort out the radiator page.
+	public static void updatePage() throws IOException, TemplateException {
+		MakePage model = new MakePage(log);
+		model.fillPage();
+	}
+}
